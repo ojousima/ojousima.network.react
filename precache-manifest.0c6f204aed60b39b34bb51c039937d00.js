87b952baefd4fd1cd0c87217855a499b0dcd499e
ojousima <otso@ojousima.net>
2020-05-07 13:11:25 +0000

    Deploying to gh-pages from  @ adecdad038aa5dd72783a34657007bc47fad03b7 🚀

---
+++ precache-manifest.0c6f204aed60b39b34bb51c039937d00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22b3850100bf63456e6ff305ee731df",
+    "url": "/ojousima.network.react/index.html"
+  },
+  {
+    "revision": "3cf026f19870d8aed965",
+    "url": "/ojousima.network.react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2755d0f955fa45285116",
+    "url": "/ojousima.network.react/static/js/2.d835adf6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ojousima.network.react/static/js/2.d835adf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf026f19870d8aed965",
+    "url": "/ojousima.network.react/static/js/main.ea1d8345.chunk.js"
+  },
+  {
+    "revision": "5abe2f601a7b166c6f12",
+    "url": "/ojousima.network.react/static/js/runtime-main.1faf8521.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ojousima.network.react/static/media/logo.5d5d9eef.svg"
+  }
+]);
